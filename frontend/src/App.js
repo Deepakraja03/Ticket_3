f8d9f9fcd5ea3885670e4218a84c48ba647e8715
nishanthspk <spknishanth2004@gmail.com>
2023-10-19 19:02:41 +0530

    Host

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home';
-import Host from './Pages/Host';
+import Host from './Pages/Host1';
 
 
 function App() {
