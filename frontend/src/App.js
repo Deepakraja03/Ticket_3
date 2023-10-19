9c4e248c64dd9ec2ccfcdeaa346d0055b5724125
Sunil_0881 <sunil12162003@gmail.com>
2023-10-19 19:02:32 +0530

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home';
 import Host from './Pages/Host';
+import Book from './Pages/Book';
+import Events from './Pages/Events';
 
 
 function App() {
@@ -11,7 +13,8 @@ function App() {
           <Routes>
           <Route path="/" Component={Home} />
           <Route path="/host" Component={Host} />
-          {/* <Route path="/book" Component={Book} /> */}
+          <Route path="/book" Component={Book} />
+          <Route path="/event" Component={Events} /> 
 
 
           </Routes>
---
+++ frontend/src/Pages/Book.jsx
@@ -0,0 +1,9 @@
+const Book = () => {
+    return ( 
+        <div className="bg-black h-screen">
+            <h1 className="text-4xl font-bold text-white pt-10 flex justify-center hover:underline">Booking Tickets</h1>
+        </div>
+     );
+}
+ 
+export default Book;
