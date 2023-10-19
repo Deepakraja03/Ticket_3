ea827ed2733b89fa3de7a0684838385e8f341c17
Sunil_0881 <sunil12162003@gmail.com>
2023-10-19 18:01:21 +0530

    home full

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,14 +1,21 @@
+
+import React from 'react';
+
+
 const Navbar = () => {
+   
+    
+    
     return ( 
         <div className="pt-12 ml-96 mr-96 px-20">
             <div className="flex justify-center text-white pt-2  pb-2  bg-white/30 rounded-full">
                 <div className="flex items-center ">
                 <h1 className="text-yellow-400 text-2xl font-bold">Ticket3</h1>
-                <a href="www.google.com" className="text-lg ml-7">Home</a>
-                <a href="www.google.com" className="text-lg ml-7">Host</a>
-                <a href="www.google.com" className="text-lg ml-7 mr-7">Book</a>
+                <a href="www.google.com" className="text-lg ml-7 hover:underline">Home</a>
+                <a href="www.google.com" className="text-lg ml-7 hover:underline">Host</a>
+                <a href="www.google.com" className="text-lg ml-7 mr-7 hover:underline">Book</a>
                 </div>
-                <button className="text-xl text-black font-semibold border-2 border-yellow-400 bg-yellow-400  rounded-2xl px-3 py-1 hover:bg-black hover:border-2 hover:text-yellow-400 hover:border-yellow-400">
+                <button  className="text-xl text-black font-semibold border-2 border-yellow-400 bg-yellow-400  rounded-2xl px-3 py-1 hover:bg-black hover:border-2 hover:text-yellow-400 hover:border-yellow-400">
                     Connect Wallet
                 </button>
             </div>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
                     <h1 className="text-7xl" >Ticket3: Crypto Tickets, Your Way</h1>
                     <p className="text-lg  mt-8 mr-28">With Ticket3, you can book your tickets using crypto and get your own ticket quickly. Embrace the future of ticketing – it's fast, secure, and entirely hassle-free.</p>
                     <button className="bg-white text-black mt-10 font-bold text-xl px-3 border-2 py-2 rounded-full hover:bg-black hover:border-2 hover:border-white hover:text-white">Book Now</button>
-                    <button className="ml-16 text-xl border-2 px-3 py-2 rounded-full hover:text-black hover:bg-white ">Learn More </button>
+                    <button className="ml-16 text-xl border-2 px-3 py-2 rounded-full hover:text-black hover:bg-white ">Host Now </button>
                 </div>
                 
             </main>
