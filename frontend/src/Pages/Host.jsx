94c2165d9871d3cef3a69b8fd0304a274524d289
Sunil_0881 <sunil12162003@gmail.com>
2023-10-20 11:17:54 +0530

    struct

--- frontend/src/Pages/Host.jsx
+++ frontend/src/Pages/Host.jsx
@@ -1,52 +1,57 @@
 import React from "react";
+import config from "../Contract/ticket.json";
+import  { useState } from "react";
+import Web3 from 'web3';
 import Navbar from "../Components/Navbar";
-import { useState } from "react"
-
 
 const Host = () => {
-  const [Description, setDescription] = useState('')
-  const [Eventname, setEventname] = useState('')
-  const [Date, setDate] = useState('')
-  const [Time, setTime] = useState('')
-  const [Venue, setVenue] = useState('')
-  const [Capacity, setCapacity] = useState('')
-  const [error, setError] = useState(null)
-  const [emptyFields, setEmptyFields] = useState([])
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
+    const [web3, setWeb3] = useState(null);
+    const [emptyFields, setEmptyFields] = useState([]);
 
-    
-
-    const host = {Description, Eventname, Date, Time, Venue}
-
-    const response = await fetch('/api/host', {
-      method: 'POST',
-      body: JSON.stringify(host),
-      headers: {
-        'Content-Type': 'application/json',
-        
-      }
-    })
-    const json = await response.json()
-
-    if (!response.ok) {
-      setError(json.error)
-      setEmptyFields(json.emptyFields)
-    }
-    if (response.ok) {
-      setDescription('')
-      setEventname('')
-      setDate('')
-      setTime('')
-      setVenue('')
-      setError(null)
-      setEmptyFields([])
+    const [Description, setDescription] = useState("");
+    const [Eventname, setEventname] = useState("");
+    const [Date, setDate] = useState("");
+    const [Time, setTime] = useState("");
+    const [Venue, setVenue] = useState("");
+    const [Capacity, setCapacity] = useState("");
+    const [error, setError] = useState(null);
   
-    }
-  }
-
-
+    const initializeWeb3 = async () => {
+      if (window.ethereum) {
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
+  
+        try {
+          await window.ethereum.enable();
+        } catch (error) {
+          console.error(error);
+        }
+      } else if (window.web3) {
+        const web3Instance = new Web3(window.web3.currentProvider);
+        setWeb3(web3Instance);
+      } else {
+        console.log("Non-Ethereum browser detected. You should consider trying MetaMask!");
+      }
+    };
+  
+    const hostEvent = async (e) => {
+      e.preventDefault();
+      const accounts = await web3.eth.getAccounts();
+  
+      try {
+        const contract = new web3.eth.Contract(config.abi);
+        await contract.methods
+          .hostEvent(Eventname, Venue, Capacity, 10, Date, Time)
+          .send({ from: accounts[0] });
+  
+        alert('Event hosted successfully!');
+      } catch (error) {
+        console.error(error);
+        setError('Error hosting event');
+      }
+    };
+ 
     return ( 
       <div className=" play bg-gray-600 h-screen">
         <Navbar />
@@ -56,7 +61,7 @@ const Host = () => {
         <div class="relative z-0 w-9/12 mb-6 group">
             <input type="text" name="description" onChange={(e) => setDescription(e.target.value)}
             value={Description}
-            className={emptyFields.includes('Description') ? 'error' : ''} id="description" class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-black dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-white" placeholder=" Description" required  />
+            className={emptyFields.includes('Description') ? 'error' : ''} id="description" class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-black dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-white" placeholder=" Description"  />
             <label for="description" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 pl-6" data-aos="fade-right"></label>
         </div> <div class="relative z-0 w-9/12 mb-6 group">
             <input type="text" onChange={(e) => setEventname(e.target.value)}
@@ -74,7 +79,7 @@ const Host = () => {
         <div class="relative z-0 w-1/3 mb-6 group">
             <input type="date" onChange={(e) => setDate(e.target.value)}
             value={Date}
-            className={emptyFields.includes('Date') ? 'error' : ''} name="date" id="date" class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-black dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-white" placeholder=" Date " required />
+            className={emptyFields.includes('Date') ? 'error' : ''} name="date" id="date" class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-black dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-white" placeholder=" Date "  />
             <label for="date" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 pl-6">To</label>
         </div>
         </div>
@@ -88,7 +93,7 @@ const Host = () => {
         <div class="relative z-0 w-1/3 mb-6 group">
             <input type="time" onChange={(e) => setTime(e.target.value)}    
             value={Time}
-            className={emptyFields.includes('Time') ? 'error' : ''} name="time" id="time" class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-black dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-white" placeholder=" Time" required />
+            className={emptyFields.includes('Time') ? 'error' : ''} name="time" id="time" class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-black dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-white" placeholder=" Time"  />
             <label for="time" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 pl-6">End</label>
         </div>
         </div>
@@ -110,7 +115,7 @@ const Host = () => {
         
         </div>
         <div class=" pl-44 ">
-            <button type="submit" class="text-black font-semibold  bg-yellow-400 hover:scale-105 focus:ring-4 focus:outline-none focus:ring-blue-300 font-md rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-yellow-300 hover:scale-105 dark:focus:ring-black" onSubmit={handleSubmit}>Host</button>
+            <button type="submit" class="text-black font-semibold  bg-yellow-400 hover:scale-105 focus:ring-4 focus:outline-none focus:ring-blue-300 font-md rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-yellow-300 hover:scale-105 dark:focus:ring-black" onSubmit={hostEvent}>Host</button>
         </div>
       </form>
       {error}
